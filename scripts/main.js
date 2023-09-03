89673ffdd070d99efe73c1589f7d86a7aaa81b36
sreekanths-24 <sreekanthsanthosh17@gmail.com>
2023-09-03 14:08:47 +0530

    testing

--- scripts/main.js
+++ scripts/main.js
@@ -3,20 +3,24 @@ const mainBtn = document.getElementById("mainbtn");
 const jsBtn = document.getElementById("jsbtn");
 const mainProjects = document.getElementById("main-projects");
 const jsProjects = document.getElementById("js-projects");
+const navItems = document.querySelectorAll(".nav-item");
 
-mainBtn.addEventListener("click", () => {
+function switchToMainProjects() {
     mainProjects.style.display = "block";
     jsProjects.style.display = "none";
     mainBtn.classList.add("active");
     jsBtn.classList.remove("active");
-});
+}
 
-jsBtn.addEventListener("click", () => {
+function switchToJSProjects() {
     mainProjects.style.display = "none";
     jsProjects.style.display = "block";
     mainBtn.classList.remove("active");
     jsBtn.classList.add("active");
-});
+}
+
+mainBtn.addEventListener("click", switchToMainProjects);
+jsBtn.addEventListener("click", switchToJSProjects);
 
 // Swipe gesture handling
 let startX;
@@ -31,9 +35,22 @@ document.addEventListener("touchend", (e) => {
 
     if (deltaX > 50) {
         // Swipe right, switch to Main Projects
-        mainBtn.click();
+        switchToMainProjects();
+        navItems.forEach(item => item.style.transform = 'translateX(0)');
     } else if (deltaX < -50) {
         // Swipe left, switch to Practice Projects
-        jsBtn.click();
+        switchToJSProjects();
+        navItems.forEach(item => item.style.transform = 'translateX(0)');
     }
+});
+
+// Add animation for left and right movement
+navItems.forEach(item => {
+    item.addEventListener("mouseover", () => {
+        item.style.transform = 'translateX(-10px)';
+    });
+
+    item.addEventListener("mouseout", () => {
+        item.style.transform = 'translateX(0)';
+    });
 });
